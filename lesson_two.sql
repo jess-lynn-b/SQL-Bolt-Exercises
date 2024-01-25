360764aa269eea2b16ea3270b09a0ac8f18c957f
Jessica Blackwell <138169206+jess-lynn-b@users.noreply.github.com>
2024-01-24 22:31:51 -0600

    lesson_two.sql

---
+++ lesson_two.sql
@@ -0,0 +1,11 @@
+SELECT * FROM movies WHERE id = 6;
+SELECT * FROM movies WHERE year BETWEEN 2000 AND 2010;
+SELECT * FROM movies WHERE year NOT BETWEEN 2000 AND 2010;
+SELECT title, year FROM movies
+WHERE year <= 2003;
+
+Exercise 2 — Tasks
+Find the movie with a row id of 6 ✓
+Find the movies released in the years between 2000 and 2010 ✓
+Find the movies not released in the years between 2000 and 2010 ✓
+Find the first 5 Pixar movies and their release year ✓
