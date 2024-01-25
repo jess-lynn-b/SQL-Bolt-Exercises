2f6616f0c922e0b5a4c8b7c6689b41fd14636bf0
Jessica Blackwell <138169206+jess-lynn-b@users.noreply.github.com>
2024-01-24 22:54:58 -0600

    lesson_3.sql

---
+++ lesson_3.sql
@@ -0,0 +1,14 @@
+SELECT title, director FROM movies 
+WHERE title LIKE "Toy Story";
+SELECT * FROM movies 
+WHERE director = "John Lasseter";
+SELECT * FROM movies 
+WHERE director NOT LIKE "John Lasseter";
+SELECT * FROM movies 
+WHERE title LIKE "WALL-";
+
+Exercise 3 — Tasks
+Find all the Toy Story movies ✓
+Find all the movies directed by John Lasseter ✓
+Find all the movies (and director) not directed by John Lasseter ✓
+Find all the WALL-* movies ✓
