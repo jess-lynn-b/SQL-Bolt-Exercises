30f88efedcc217f8a06fd861bba073d854e41edc
Jessica Blackwell <138169206+jess-lynn-b@users.noreply.github.com>
2024-01-24 22:17:57 -0600

    lesson_one.sql

---
+++ lesson_one.sql
@@ -0,0 +1,12 @@
+SELECT title FROM movies; 
+SELECT director FROM movies;
+SELECT title, director FROM movies;
+SELECT title, year FROM movies;
+SELECT * FROM movies;
+
+Exercise 1 — Tasks
+Find the title of each film ✓
+Find the director of each film ✓
+Find the title and director of each film ✓
+Find the title and year of each film ✓
+Find all the information about each ✓
